ae2f11afdd52258289be0a3becbf5478a6671c56
hans <hmrtn@pm.me>
2022-09-20 17:41:52 -0700

    fixed: qv contract factory create tests

--- packages/contracts/test/QVFactory.test.ts
+++ packages/contracts/test/QVFactory.test.ts
@@ -2,100 +2,134 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { deployContract } from "ethereum-waffle";
 import { isAddress } from "ethers/lib/utils";
-import { artifacts, ethers } from "hardhat";
+import { artifacts, ethers, upgrades } from "hardhat";
 import { Artifact } from "hardhat/types";
-import { QVFactory, QVImplementation, VoterRegister } from "../typechain";
-import { BigNumber, utils } from "ethers";
+import {
+  QVFactory,
+  QVFactory__factory,
+  QVImplementation,
+  VoterRegister,
+} from "../typechain";
+import { BigNumber, utils, Wallet } from "ethers";
+import { AddressZero } from "@ethersproject/constants";
 
-describe("QVFactory", function () {
-  let user0: SignerWithAddress;
-  let user1: SignerWithAddress;
-  let QVFactory: QVFactory;
-  let QVFactoryArtifact: Artifact;
-  let QVImplementation: QVImplementation;
-  let QVImplementationArtifact: Artifact;
+const encoder = new utils.AbiCoder();
+
+const encodeParameters = (
+  _voteCredits: number,
+  _voterRegister: string,
+  _adminRoles: string[],
+  _roundOperators: string[]
+): string => {
+  return encoder.encode(
+    ["tuple(uint256, address, address[], address[])"],
+    [[_voteCredits, _voterRegister, _adminRoles, _roundOperators]]
+  );
+};
+
+describe("QVFactory", () => {
+  let user: SignerWithAddress;
+
+  // QV Factory
+  let qvFactory: QVFactory;
+  // eslint-disable-next-line camelcase
+  let qvContractFactory: QVFactory__factory;
+
+  // QV Implementation
+  let qvImplementation: QVImplementation;
+  let qvImplementationArtifact: Artifact;
+
+  // Voter Register
   let VoterRegister: VoterRegister;
   let VoterRegisterArtifact: Artifact;
-  const encoder = new utils.AbiCoder();
-
-  const encodeParameters = (
-    _voteCredits: number,
-    _voterRegister: string,
-    _adminRoles: string[],
-    _roundOperators: string[]
-  ): string => {
-    return encoder.encode(
-      ["tuple(uint256, address, address[], address[])"],
-      [[_voteCredits, _voterRegister, _adminRoles, _roundOperators]]
-    );
-  };
 
   describe("constructor", () => {
-    it("deploys properly", async () => {
-      [user0, user1] = await ethers.getSigners();
+    it("QVFactory SHOULD deploy properly", async () => {
+      [user] = await ethers.getSigners();
 
-      QVFactoryArtifact = await artifacts.readArtifact("QVFactory");
-      QVFactory = <QVFactory>await deployContract(user0, QVFactoryArtifact, []);
+      qvContractFactory = await ethers.getContractFactory("QVFactory");
+      qvFactory = <QVFactory>await qvContractFactory.deploy();
 
       // Verify deploy
       // eslint-disable-next-line no-unused-expressions
-      expect(isAddress(QVFactory.address), "Failed to deploy QVFactory").to.be
+      expect(isAddress(qvFactory.address), "Failed to deploy QVFactory").to.be
         .true;
     });
   });
 
   describe("core functions", () => {
-    before(async () => {
-      [user0, user1] = await ethers.getSigners();
+    beforeEach(async () => {
+      [user] = await ethers.getSigners();
 
       // Deploy QVFactory contract
-      QVFactoryArtifact = await artifacts.readArtifact("QVFactory");
-      QVFactory = <QVFactory>await deployContract(user0, QVFactoryArtifact, []);
+      qvContractFactory = await ethers.getContractFactory("QVFactory");
+      qvFactory = <QVFactory>await upgrades.deployProxy(qvContractFactory);
 
-      QVFactory.initialize();
       // Deploy QVImplementation contract
-      QVImplementationArtifact = await artifacts.readArtifact(
+      qvImplementationArtifact = await artifacts.readArtifact(
         "QVImplementation"
       );
-      QVImplementation = <QVImplementation>(
-        await deployContract(user0, QVImplementationArtifact, [])
+      qvImplementation = <QVImplementation>(
+        await deployContract(user, qvImplementationArtifact, [])
       );
-
+      console.log(qvImplementation);
       // Deploy Voter register contract
       VoterRegisterArtifact = await artifacts.readArtifact("VoterRegister");
       VoterRegister = <VoterRegister>(
-        await deployContract(user0, VoterRegisterArtifact, [
+        await deployContract(user, VoterRegisterArtifact, [
           "TEST",
           "TEST",
           "TEST",
         ])
       );
     });
+  });
 
-    // Update QVImplementation contract address in QVFactory
-    describe("test: updateQVContract", () => {
-      it("should update qv contract address", async () => {
-        expect(QVFactory.updateQVContract(QVImplementation.address))
-          .to.emit(QVFactory, "QVContractUpdated")
-          .withArgs(QVImplementation.address);
-      });
+  describe("test: updateQVContract", async () => {
+    it("QVContract SHOULD have default address after deploy ", async () => {
+      expect(await qvFactory.qvContract()).to.be.equal(AddressZero);
     });
 
-    describe("test: create", () => {
-      // it("should create a new implementation of qv", async () => {
-      //   console.log(VoterRegister.address);
-      //   const encodedParams = encodeParameters(
-      //     100,
-      //     VoterRegister.address,
-      //     [user0.address],
-      //     [user0.address]
-      //   );
-      //   await QVFactory.create(encodedParams, user0.address);
-      // await expect(QVFactory.create(encodedParams, user1.address)).to.emit(
-      //   QVFactory,
-      //   "QVCreated"
-      // );
-      // });
+    // TODO: Test the non-zero address
+    it("QVContract SHOULD emit after invoking updateQVContract", async () => {
+      qvFactory.initialize();
+      expect(qvFactory.connect(user).updateQVContract(AddressZero))
+        .to.emit(qvFactory, "QVContractUpdated")
+        .withArgs(AddressZero);
+    });
+
+    // TODO: Test the non-zero address
+    it("QVContract SHOULD be updated after updateQVContract", async () => {
+      await qvFactory.updateQVContract(AddressZero);
+      const qvContract = await qvFactory.qvContract();
+      expect(qvContract).to.be.equal(AddressZero);
+    });
+  });
+
+  describe("test: create", () => {
+    it("QVContract SHOULD create a new implementation of qv", async () => {
+      const encodedParams = encodeParameters(
+        100,
+        AddressZero,
+        [user.address],
+        [user.address]
+      );
+      const txn = await qvFactory.create(encodedParams, AddressZero);
+      const receipt = await txn.wait();
+      // eslint-disable-next-line no-unused-expressions
+      expect(txn.hash).to.not.be.empty;
+      expect(receipt.status).equals(1);
+    });
+    it("QVContract SHOULD emit QVCreated after invoking create", async () => {
+      const encodedParams = encodeParameters(
+        100,
+        AddressZero,
+        [user.address],
+        [user.address]
+      );
+      const txn = await qvFactory.create(encodedParams, AddressZero);
+
+      expect(txn).to.emit(qvFactory, "QVCreated");
     });
   });
 });
